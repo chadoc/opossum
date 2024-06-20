c03d301703935a5c83147e7889c796a9ef73bc3f
Julien Repond <julien.repond@gmail.com>
2024-06-20 22:22:10 +0200

    fix: center info bulle

--- src/components/game/FullGame.vue
+++ src/components/game/FullGame.vue
@@ -133,6 +133,10 @@ function restart() {
   max-height: 40px;
 }
 .agenda-container {
-  margin-top: 60px;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
 }
 </style>
